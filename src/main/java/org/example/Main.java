389c1d4ad8c80e317aadee87f292848c6682837a
alisengul53 <64622169+alisengul53@users.noreply.github.com>
2022-11-03 01:05:50 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,16 @@ public class Main {
     System.out.println("Giriş yapıldı");
     Event bjkEvent = apiCalls.getEventByName(System.getenv("matchName"));
     EventDetailsDTO eventDetailsDTO = apiCalls.getEventDetailsById(bjkEvent);
-    Collections.reverse(eventDetailsDTO.value.categories);
+    String str = System.getenv("desiredList");
+    List<String> items = Arrays.asList(str.split("\\s*,\\s*"));
+    List<EventDetail> desiredList = new ArrayList<>();
+    int listSize = eventDetailsDTO.value.categories.size();
+    for(int t = 0; t < listSize; t++)
+    {
+      if(items.contains(String.valueOf(t+1)))
+        desiredList.add(eventDetailsDTO.value.categories.get(t));
+    }
+    eventDetailsDTO.value.categories = desiredList;
     AvailableBlocksDTO availableBlocksDTO;
     AddToChartRequestDTO addToChartRequestDTO = new AddToChartRequestDTO();
     boolean giris = false;
