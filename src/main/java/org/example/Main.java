8421c6a315b9bd9d4a967cd5cb291780a64eb323
alisengul53 <64622169+alisengul53@users.noreply.github.com>
2022-11-03 01:11:29 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import org.DTOs.*;
 import org.apiCalls.ApiCalls;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
